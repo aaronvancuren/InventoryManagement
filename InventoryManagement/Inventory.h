669d3d36f02c7971b65266836aa179c4a70bbef8
Aaron Van Curen <99290929+vancurenaaron@users.noreply.github.com>
2023-07-30 20:46:33 -0400

    started adding logic for pulling data from inventory file

---
+++ InventoryManagement/Inventory.h
@@ -0,0 +1,172 @@
+#pragma once
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <list>
+#include <vector>
+
+#include "CPU.h"
+#include "GPU.h"
+#include "Monitor.h"
+#include "Motherboard.h"
+#include "PowerSupply.h"
+#include "Product.h"
+#include "RAM.h"
+#include "Storage.h"
+
+// Namespaces
+namespace fs = std::filesystem;
+
+class Inventory
+{
+private:
+    std::list<Product*> products;
+
+    std::vector<std::string> ParseInventoryFile(std::string productText)
+    {
+        std::vector<std::string> productProperties;
+        do
+        {
+            int startIndex = 0;
+            int endIndex = 0;
+            if (productText[startIndex] == '[')
+            {
+                endIndex = productText.find(']');
+                std::string productProperty = productText.substr(startIndex + 1, endIndex - 1);
+                productProperties.push_back(productProperty);
+                productText = productText.substr(endIndex + 1, productText.size());
+            }
+            else if (productText[startIndex] == '{')
+            {
+                endIndex = productText.find('}');
+                std::string productListProperty = productText.substr(startIndex + 1, endIndex - 1);
+                std::vector<std::string> nestedProperties = ParseInventoryFile(productListProperty);
+                productText = productText.substr(endIndex + 1, productText.size());
+
+                productListProperty = "";
+                for (int i = 0; i < nestedProperties.size(); i++)
+                {
+                    productListProperty.append(nestedProperties[i]);
+                    if (i < nestedProperties.size() - 1)
+                    {
+                        productListProperty.append(",");
+                    }
+                }
+
+                productProperties.push_back(productListProperty);
+            }
+        } while (!productText.empty());
+
+        return productProperties;
+    }
+
+    std::vector<std::string> ParsePropertyList(std::string propertyList)
+    {
+        std::vector<std::string> properties;
+        do
+        {
+            int startIndex = 0;
+            int endIndex = 0;
+            endIndex = propertyList.find(',');
+            properties.push_back(propertyList.substr(startIndex, endIndex - 1));
+            propertyList = propertyList.substr(endIndex + 1, propertyList.size());
+        } while (!propertyList.empty());
+
+        return properties;
+    }
+
+public:
+    Inventory(fs::path inventoryFilePath)
+    {
+        std::ifstream inventoryFile;
+        if (!inventoryFile.is_open())
+        {
+            inventoryFile.open(inventoryFilePath);
+        }
+
+        if (inventoryFile.is_open())
+        {
+            while (inventoryFile)
+            {
+                std::string productText;
+                std::getline(inventoryFile, productText, ';');
+                std::vector<std::string> productProperties = ParseInventoryFile(productText);
+
+                Product* product = 0;
+                std::string productType = productProperties[0];
+                // Index 1 - 6 are Product parameters
+                if (productType == "class CPU")
+                {
+                    product = new CPU(productProperties[1], productProperties[2], productProperties[3], productProperties[4], productProperties[5], stof(productProperties[6]),
+                        stoi(productProperties[7]), stoi(productProperties[8]), stof(productProperties[9]));
+                }
+                else if (productType == "class GPU")
+                {
+                    std::vector<std::string> displayOutputs = ParsePropertyList(productProperties[8]);
+                    product = new GPU(productProperties[1], productProperties[2], productProperties[3], productProperties[4], productProperties[5], stof(productProperties[6]),
+                        stoi(productProperties[7]), displayOutputs);
+                }
+                else if (productType == "class Monitor")
+                {
+                    std::vector<std::string> displayOutputs = ParsePropertyList(productProperties[8]);
+                    product = new Monitor(productProperties[1], productProperties[2], productProperties[3], productProperties[4], productProperties[5], stof(productProperties[6]),
+                        stof(productProperties[7]), stoi(productProperties[8]), productProperties[9], displayOutputs);
+                }
+                else if (productType == "class Motherboard")
+                {
+                    product = new Motherboard(productProperties[1], productProperties[2], productProperties[3], productProperties[4], productProperties[5], stof(productProperties[6]),
+                        stof(productProperties[7]), productProperties[8], stoi(productProperties[9]), stoi(productProperties[10]), stoi(productProperties[11]));
+                }
+                else if (productType == "class PowerSupply")
+                {
+                    product = new PowerSupply(productProperties[1], productProperties[2], productProperties[3], productProperties[4], productProperties[5], stof(productProperties[6]),
+                        stoi(productProperties[7]), productProperties[8], stoi(productProperties[9]));
+                }
+                else if (productType == "class RAM")
+                {
+                    product = new RAM(productProperties[1], productProperties[2], productProperties[3], productProperties[4], productProperties[5], stof(productProperties[6]),
+                        stof(productProperties[7]), productProperties[8], stoi(productProperties[9]));
+                }
+                else if (productType == "class Storage")
+                {
+                    product = new Storage(productProperties[1], productProperties[2], productProperties[3], productProperties[4], productProperties[5], stof(productProperties[6]),
+                        productProperties[7], stoi(productProperties[8]));
+                }
+                else
+                {
+                    std::cout << "Invalid product listings" << std::endl;
+                    continue;
+                }
+
+                products.push_back(product);
+            }
+        }
+    }
+
+    std::list<Product*> getProducts()
+    {
+        return products;
+    }
+
+    void addProduct(Product& product)
+    {
+        products.push_back(&product);
+    }
+
+    void removeProduct(Product& product)
+    {
+        products.remove(&product);
+    }
+
+    double inventoryCarryingCost()
+    {
+        double totalCost = 0;
+        for(Product* product : products)
+        {
+            totalCost += product->getPrice();
+        }
+
+        return totalCost;
+    }
+};
+
--- InventoryManagement/InventoryManagement.cpp
+++ InventoryManagement/InventoryManagement.cpp
@@ -1,20 +1,121 @@
-// InventoryManagement.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Program name: InventoryManagement.cpp
+// Author: Aaron Van Curen
+// Date last updated: 07/29/2023
+// Purpose: Allows a user to manage existing inventory files by showing, adding, and removing products.
 
+// Library & File References
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <list>
+#include <string>
+
+#include "CPU.h"
+#include "EnumHelpers.h"
+#include "GPU.h"
+#include "Inventory.h"
+#include "Monitor.h"
+#include "Motherboard.h"
+#include "PowerSupply.h"
+#include "Product.h"
+#include "RAM.h"
+#include "Storage.h"
+
+// Namespaces
+namespace fs = std::filesystem;
+
+// Constant Variables
+const fs::path inventoryPath = fs::current_path().parent_path()/"Inventories";
+
+// Method Declarations
+fs::path getSelectedInventoryFilePath();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    fs::path inventoryFilePath = getSelectedInventoryFilePath();
+    Inventory selectedInventory(inventoryFilePath);
+
+    std::string menuInput;
+    do
+    {
+        std::cout << "1. Exit" << std::endl;
+        std::cout << "2. Show Available Products" << std::endl;
+        std::cout << "3. Purchase A Product" << std::endl;
+        std::cout << "4. Sell A Product" << std::endl;
+        std::cin >> menuInput;
+        try
+        {
+            switch (stoi(menuInput))
+            {
+            case 1:
+                exit(0);
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+                throw std::invalid_argument("Invalid Selection");
+            }
+        }
+        catch (const std::invalid_argument e)
+        {
+            std::cout << "Please enter a valid number corresponding to a menu option. Error: " << e.what() << std::endl;
+        }
+        catch (const std::out_of_range e)
+        {
+            std::cout << "Please enter a valid number corresponding to a menu option. Error:" << e.what() << std::endl;
+        }
+        catch (const std::exception& e)
+        {
+            std::cout << e.what() << std::endl;
+        }
+        catch (...)
+        {
+            exit(-1);
+        }
+    } while (menuInput != "1");
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+/// <summary>
+/// Returns the selected inventory file path that contains products
+/// </summary>
+/// <returns></returns>
+fs::path getSelectedInventoryFilePath()
+{
+    // Start of menu selection
+    std::cout << "Select an inventory" << std::endl;
+    std::cout << "1. Exit" << std::endl;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    // Print the list of available inventory files that contain products
+    int i = 2;
+    std::vector<fs::path> inventoryOptions;
+    for (const fs::directory_entry file : fs::directory_iterator(inventoryPath))
+    {
+        inventoryOptions.push_back(file.path());
+        std::cout << i << ". " << file.path().filename() << std::endl;
+        i++;
+    }
+
+    // File path of the selected inventory
+    std::string inventorySelection;
+    std::cin >> inventorySelection;
+    try
+    {
+        // Exiting will return an empty file stream
+        if (stoi(inventorySelection) == 1)
+        {
+            exit(1);
+        }
+
+        // File stream of selected inventory
+        return inventoryOptions[stoi(inventorySelection) - 2];
+    }
+    catch (const std::exception&)
+    {
+        // Reprompts the user
+        std::cout << "Invalid inventory selection" << std::endl;
+        return getSelectedInventoryFilePath();
+    }
+}
